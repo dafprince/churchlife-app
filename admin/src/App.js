1ddf7bd6e2c604aa58c037a490ce3a4664106e54
dafonseca <onesimedafonseca@gmail.com>
2025-09-13 04:47:51 +0000

    Fix unused import in App.js

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,7 +1,7 @@
 // src/App.js
 import React from 'react';
 import './App.css';
-import AdminUsers from './pages/AdminUsers';
+//import AdminUsers from './pages/AdminUsers';
 import DashboardLayout from './components/DashboardLayout';
 
 function App() {
